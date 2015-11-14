d29ace1724fcb1eca09f0800a1b3a3574e1df957
Sam Kreter <samkreter@gmail.com>
2015-11-14 12:57:12 -0600

    creating truck parsingk

--- como-on-fire.py
+++ como-on-fire.py
@@ -13,19 +13,20 @@ for node in tree.iter('item'):
     pubDate = node[0].text
     title = node[1].text
     desc = node[2].text
-    geolat = node[3].text
-    geolong = node[4].text
+    geolat = float(node[3].text)
+    geolong = float(node[4].text)
     callDateTime = node[5].text
     address = node[6].text
     displayName = node[7].text
     in_id = node[8].text
-    timestamp = node[9].text
-    callDatalat = node[10].text
-    callDatalong = node[11].text
+    timestamp = int(node[9].text)
+    callDatalat = float(node[10].text)
+    callDatalong = float(node[11].text)
     agency = node[12].text
     FDids = node[13].text
-    trucks = node[14].text.split(" - ")
-    print trucks
+    trucks = node[14].text.split(" - ") if node[14].text is not None else " "
+    for truck in trucks:
+      session.run("MERGE (a:Truck {id: {truck}})",{'truck':truck})
     session.run("""MERGE (a:Item {id: {in_id}})
              set a.pubDate={pubDate},
                  a.title={title},
@@ -39,8 +40,7 @@ for node in tree.iter('item'):
                  a.callDatalat={callDatalat},
                  a.callDatalong={callDatalong},
                  a.agency={agency},
-                 a.FDids={FDids},
-                 a.trucks={trucks}
+                 a.FDids={FDids}
                  """, {'in_id':in_id,
                    'pubDate':pubDate,
                    'title':title,
@@ -54,8 +54,7 @@ for node in tree.iter('item'):
                    'callDatalat':callDatalat,
                    'callDatalong':callDatalong,
                    'agency':agency,
-                   'FDids':FDids,
-                   'trucks':trucks})
+                   'FDids':FDids})
 
 
 
