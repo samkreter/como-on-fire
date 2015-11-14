ecc6e768d2ae6cac38d98fbd511118387d04d0a7
Ubuntu <ubuntu@ip-10-0-0-215.ec2.internal>
2015-11-14 16:43:21 +0000

    working on db queries

--- como-on-fire.py
+++ como-on-fire.py
@@ -3,20 +3,19 @@ from neo4j import GraphDatabase, Node, Relationship, Path, CypherError
 import urllib2
 
 
+session = GraphDatabase.driver("bolt://localhost").session()
+
 result = urllib2.urlopen('https://www.gocolumbiamo.com/PSJC/Services/911/911dispatch/fire_georss.php').read()
 tree = ElementTree.fromstring(result)
 	
 
 for node in tree.iter('item'):
-	print node[2].text  
+    title = node[1].text
+    desc = node[2].text  
 #url = node.attrib.get('xmlUrl')
 
 
-#session = GraphDatabase.driver("bolt://localhost").session()
-
-#driver = neo4j.driver("bolt://localhost")
-#session = driver.session()
-#session.run("CREATE (a:Person {name:'Bob'})")
+session.run("""MERGE (a:Item {id: {in_id} })""", in_id=in_id) on 
 #for name, in session.run("MATCH (a:Person) RETURN a.name AS name"):
 #    print(name)
 #session.close()
