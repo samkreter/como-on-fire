99cdd5297db1e4c343c2c13ec785ee08e103b48e
Sam Kreter <samkreter@gmail.com>
2015-11-14 14:44:47 -0600

    changing to dict

--- como-on-fire.py
+++ como-on-fire.py
@@ -23,60 +23,25 @@ def getTruckType(truck):
     elif truck[0] == "M":
       return "Medic"
 
-def getStationNum(truck):
-    if truck == "" or truck is None:
-	return ""
-    digit = ""
-    for s in truck:
-        if s.isdigit():
-            digit += s
-    return int(digit) if digit != "" else -1
-
 
 for node in tree.iter('item'):
-    pubDate = node[0].text
-    title = node[1].text
-    desc = node[2].text
-    geolat = float(node[3].text)
-    geolong = float(node[4].text)
-    callDateTime = node[5].text
-    address = node[6].text
-    displayName = node[7].text
-    in_id = node[8].text
-    timestamp = int(node[9].text)
-    callDatalat = float(node[10].text)
-    callDatalong = float(node[11].text)
-    agency = node[12].text
-    FDids = node[13].text
+    params = {'pubDate': node[0].text,
+    'title': node[1].text,
+    'desc':node[2].text
+    'geolat':float(node[3].text)
+    'geolong':float(node[4].text)
+    'callDateTime':node[5].text
+    'address':node[6].text
+    'displayName':node[7].text
+    'in_id':node[8].text
+    'timestamp':int(node[9].text)
+    'callDatalat':float(node[10].text)
+    'callDatalong':float(node[11].text)
+    'agency':node[12].text
+    'FDids':node[13].text}
     trucks = node[14].text.split(" - ") if node[14].text is not None else " "
     session.run("""MERGE (a:Item {id: {in_id}})
-             set a.pubDate={pubDate},
-                 a.title={title},
-                 a.desc={desc},
-                 a.geolat={geolat},
-                 a.geolong={geolong},
-                 a.callDateTime={callDateTime},
-                 a.address={address},
-                 a.displayName={displayName},
-                 a.timestamp={timestamp},
-                 a.callDatalat={callDatalat},
-                 a.callDatalong={callDatalong},
-                 a.agency={agency},
-                 a.FDids={FDids}
-                 """, {'in_id':in_id,
-                   'pubDate':pubDate,
-                   'title':title,
-                   'desc':desc,
-                   'geolat':geolat,
-                   'geolong':geolong,
-                   'callDateTime':callDateTime,
-                   'address':address,
-                   'displayName':displayName,
-                   'timestamp':timestamp,
-                   'callDatalat':callDatalat,
-                   'callDatalong':callDatalong,
-                   'agency':agency,
-                   'FDids':FDids})
+             set+={params} """,params)
     for truck in trucks:
         truckType = getTruckType(truck)
         truckStatNum = getStationNum(truck)
@@ -86,7 +51,7 @@ for node in tree.iter('item'):
                      MERGE (truck)-[:Dispatch]->(item)
                      MERGE (fireStation:FireStation {id:{truckStatNum}})
                      MERGE (truck)-[:BelongsTo]->(fireStation)
-                     """,{'truck':truck,'in_id':in_id,'truckType':truckType,'truckStatNum':truckStatNum})
+                     """,{'truck':truck,'in_id':params.in_id,'truckType':truckType,'truckStatNum':truckStatNum})
 
 
 
